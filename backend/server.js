e3911ceef11b18d7d0c515ee01ff4f1c72ae0da6
priyanka balla <priyanka.balla@glarimy.com>
2021-03-13 12:53:22 +0530

    https://github.com/priya0120/ss

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/shopping20', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
